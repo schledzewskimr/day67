fa0783c6771f6e80290fe0f46958ecbb8b0bd835
Mark Schledzewski <mark.schledzewski@gmail.com>
2022-06-01 10:12:22 -0400

    payroll

---
+++ Payroll/src/main/java/com/example/Payroll/OrderNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.Payroll;
+
+
+class OrderNotFoundException extends RuntimeException {
+
+    OrderNotFoundException(Long id) {
+        super("Could not find order " + id);
+    }
+}
---
+++ Payroll/src/main/java/com/example/Payroll/Status.java
@@ -0,0 +1,8 @@
+package com.example.Payroll;
+
+public enum Status {
+
+    IN_PROGRESS, //
+    COMPLETED, //
+    CANCELLED
+}
